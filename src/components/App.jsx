968b689ef5386492135b93015b894bfe039dad3f
nastyaclifford <madchen95@mail.ru>
2023-07-25 11:56:52 -0600

    added key

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,18 +6,19 @@ import "../styles/index.scss";
 function App() {
   return (
     <div className="container">
-      {rates.map ((item) =>(
+      {rates.map ((item, index) =>(
       <Rate 
-      name = {item.name}
-      price = {item.price}
-      speed = {item.speed}
+      name = {item.name} 
+      price = {item.price} 
+      speed = {item.speed} 
       description = {item.description}
       isSelected = {item.isSelected}
       index = {item.index}
       blueTheme = {item.blueTheme}
       greenTheme = {item.greenTheme}
       orangeTheme = {item.orangeTheme}
-      blackTheme = {item.blackTheme}/>))}
+      blackTheme = {item.blackTheme}
+      key = {index}/>))}
     </div>
   );
 }
--- src/components/Rate.jsx
+++ src/components/Rate.jsx
@@ -4,17 +4,17 @@ export default function rateCards(props) {
     
         return(
             <div>
-                <div className = "list_item">   
-                    <div className = {"card " + (props.isSelected? "selected" : "")} key ={props.index}>
-                    <div className = {"card-text " + (props.blueTheme? "blueTheme" : "") + (props.greenTheme? "greenTheme" : "") + (props.orangeTheme? "orangeTheme" : "") + (props.blackTheme? "blackTheme" : "")}> 
-                    <div className="title"><h2 className="item">{props.name}</h2>
+                <div className = "list_item" >   
+                    <div className = {"card " + (props.isSelected? "selected" : "")} >
+                    <div className = {"card-text " + (props.blueTheme? "blueTheme" : "") + (props.greenTheme? "greenTheme" : "") + (props.orangeTheme? "orangeTheme" : "") + (props.blackTheme? "blackTheme" : "") } > 
+                    <div className="title" ><h2 className="item" >{props.name}</h2>
                     </div>
-                        <div  className="item"> <div className="item-price">{props.price}</div> руб/мес</div>
+                        <div  className="item" > <div className="item-price" >{props.price}</div> руб/мес</div>
                         </div>
-                        <div className = "item-speed">
-                            <div  className="item speed">до {props.speed} Мбит/сек</div>
-                            </div><div className = "item-info">
-                        <div  className="item info">{props.description}</div> </div>
+                        <div className = "item-speed" >
+                            <div  className="item speed" >до {props.speed} Мбит/сек</div>
+                            </div><div className = "item-info" >
+                        <div  className="item info" >{props.description}</div> </div>
                         
                         </div>
                 
