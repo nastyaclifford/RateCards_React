8411c19d7d68c51f4a4d1fb57b5e6d733d2441ad
nastyaclifford <madchen95@mail.ru>
2023-07-28 14:20:35 -0600

    added useState

--- src/components/Rate.jsx
+++ src/components/Rate.jsx
@@ -1,11 +1,15 @@
+import React, {useState} from "react";
 import "../styles/Cards.scss"
 
-export default function rateCards(props) {
+export default function RateCards(props) {
+    const [selected, setSelected] = useState (false);
+    const handleChange = () => {
+        setSelected (!selected)}
     
         return(
             <div>
                 <div className = "list_item" >   
-                    <div className = {"card " + (props.isSelected? "selected" : "")} >
+                    <div onClick={handleChange} className = {selected? "card selected" : "card"} > 
                     <div className = {"card-text " + (props.blueTheme? "blueTheme" : "") + (props.greenTheme? "greenTheme" : "") + (props.orangeTheme? "orangeTheme" : "") + (props.blackTheme? "blackTheme" : "") } > 
                     <div className="title" ><h2 className="item" >{props.name}</h2>
                     </div>
